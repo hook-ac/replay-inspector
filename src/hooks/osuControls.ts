b710c29880dbe318d4411ce342f4a2a82a81f4de
cunev <cunevnew@gmail.com>
2024-01-21 18:29:09 +0200

    wheel controls

--- src/hooks/osuControls.ts
+++ src/hooks/osuControls.ts
@@ -6,6 +6,15 @@ const keyPressed = () => {
   if (p.keyCode == 32) OsuRenderer.setPlaying(!OsuRenderer.playing);
 };
 
+const mouseWheel = (event: WheelEvent) => {
+  if (event.deltaY > 0) {
+    OsuRenderer.setTime(OsuRenderer.time + 35);
+  } else {
+    OsuRenderer.setTime(OsuRenderer.time - 35);
+  }
+};
+
 export function hookOsuControls() {
   hook({ event: Events.keyPressed, callback: keyPressed });
+  hook({ event: Events.mouseWheel, callback: mouseWheel });
 }
